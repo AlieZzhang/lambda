a50caee732d99e30378d26c8ccb3c4aa3e90de56
RiceGG <1948424125@qq.com>
2018-06-07 13:56:51 +0800

    commit anytime

---
+++ src/stream/IntStreamDemo.java
@@ -0,0 +1,14 @@
+package stream;
+
+import java.util.stream.IntStream;
+
+public class IntStreamDemo {
+	public static void main(String[] args) {
+		//体验range和rangeClosed的区别
+		IntStream r1 = IntStream.range(30, 50);
+		r1.forEach(System.out::print);
+		System.out.println();
+		IntStream rc2 = IntStream.rangeClosed(30, 50);
+		rc2.forEach(System.out::print);
+	}
+}
